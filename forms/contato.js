847c8580da20f60e3ee9c574fda5b9fe7cf3a257
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2024-10-08 08:40:28 -0300

    fix: formulario de contato

--- forms/contato.js
+++ forms/contato.js
@@ -1,14 +1,20 @@
+// Inicialize o EmailJS com seu User ID
+(function() {
+    emailjs.init({
+        publicKey: "vkVPvdnj9gg8jmDkc",
+}); // Substitua "SEU_USER_ID" pelo seu User ID do EmailJS
+})();
 
 document.getElementById('contact-form').addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    
-    
+    event.preventDefault();
+    console.log('Formulário enviado'); // Para verificar se o formulário foi enviado
+
     const from_name = document.getElementById('name-field').value;
     const from_email = document.getElementById('email-field').value;
     const from_celular = document.getElementById('celular-field').value;
     const message = document.getElementById('message-field').value;
 
-    // Envia o email usando EmailJS
+    // Envio do email
     emailjs.send("service_p6jdjpi", "template_ulklo4c", {
         from_name: from_name,
         from_email: from_email,
@@ -18,8 +24,10 @@ document.getElementById('contact-form').addEventListener('submit', function(even
     .then(function(response) {
         console.log('Email enviado com sucesso!', response.status, response.text);
         alert('Mensagem enviada com sucesso!');
+        document.getElementById('contact-form').reset();
     }, function(error) {
         console.error('Falha ao enviar o email:', error);
         alert('Falha ao enviar a mensagem. Tente novamente mais tarde.');
     });
 });
+
